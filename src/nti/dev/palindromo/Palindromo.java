7041e095f4283a7b3f60d928c01a258d6458a551
Dyego Lima <dyegodll18@gmail.com>
2023-11-25 14:42:07 -0300

    Palíndromo

--- src/nti/dev/palindromo/Palindromo.java
+++ src/nti/dev/palindromo/Palindromo.java
@@ -7,7 +7,7 @@ public class Palindromo {
 	public static void main(String[] args) {
 		Scanner sc = new Scanner(System.in);
 		
-		System.out.print("Informe uma Palavra para verificação: ");
+		System.out.print("Informe uma Palavra: ");
 		String palavra = sc.nextLine();
 		
 		boolean check = palindromoCheck(palavra);
@@ -19,19 +19,19 @@ public class Palindromo {
 	
 	static boolean palindromoCheck(String palavra) {
 		
-		String polindromo ="";
-		System.out.println("Palavra = "+palavra);
+		String palindromo ="";
+		System.out.println("\nPalavra = "+palavra);
 		
 		for(int i = palavra.length() -1; i >=0; i--) {
-			polindromo += palavra.charAt(i);
+			palindromo += palavra.charAt(i);
 		}
-		System.out.println("Políndromo = "+polindromo);
+		System.out.println("Palíndromo = "+palindromo);
 		
-		if(palavra.trim().toLowerCase().equals(polindromo.trim().toLowerCase())) {
-			System.out.println("\nA palavra "+palavra+" é um Políndromo");
+		if(palavra.trim().toLowerCase().equals(palindromo.trim().toLowerCase())) {
+			System.out.println("\nA palavra "+palavra+" é um Palíndromo");
 			return true;
 		}
-		System.out.println("\nA palavra "+palavra+" não é um Políndromo");
+		System.out.println("\nA palavra "+palavra+" não é um Palíndromo");
 		return false;
 	}
 
