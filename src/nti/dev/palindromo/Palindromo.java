76f0a8bea018ce164bdb004a5d28edf60e680a6d
Dyego Lima <dyegodll18@gmail.com>
2023-11-25 14:31:54 -0300

    Políndromo

--- src/nti/dev/palindromo/Palindromo.java
+++ src/nti/dev/palindromo/Palindromo.java
@@ -1,9 +1,39 @@
 package nti.dev.palindromo;
 
+import java.util.Scanner;
+
 public class Palindromo {
 
 	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
 		
+		System.out.print("Informe uma Palavra para verificação: ");
+		String palavra = sc.nextLine();
+		
+		boolean check = palindromoCheck(palavra);
+		System.out.println(check);
+		
+		sc.close();
 	}
+
+	
+	static boolean palindromoCheck(String palavra) {
+		
+		String polindromo ="";
+		System.out.println("Palavra = "+palavra);
+		
+		for(int i = palavra.length() -1; i >=0; i--) {
+			polindromo += palavra.charAt(i);
+		}
+		System.out.println("Políndromo = "+polindromo);
+		
+		if(palavra.trim().toLowerCase().equals(polindromo.trim().toLowerCase())) {
+			System.out.println("\nA palavra "+palavra+" é um Políndromo");
+			return true;
+		}
+		System.out.println("\nA palavra "+palavra+" não é um Políndromo");
+		return false;
+	}
+
 
 }
